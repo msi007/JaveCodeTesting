588519407419ad7510821e539806fa89297e5bd9
Md. Saiful Islam <saifcse05@gmail.com>
2021-10-09 22:42:41 +0600

    OddEven,java added

---
+++ OddEven.java
@@ -0,0 +1,25 @@
+package ODDEven;
+import java.util.*;
+import java.util.Scanner;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		int i;
+		Scanner input=new Scanner(System.in);  //Scanner class is used to collect user input
+		//System.out.print("Idenfty Odd-Even Number ulti:");
+		//int num=input.nextInt();
+		for (i=0; i<=10;i++) {
+			
+			if (i%2==0)
+				System.out.print(i+ " is a Even number\n");
+			else 
+				System.out.print(i+ "  is Odd number\n");
+			
+		}
+		
+			
+
+	}
+
+}
